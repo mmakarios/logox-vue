4fa811d5e38619e501b4d125f6afe8fe74279a13
mmakarios <michel.makarios@gmail.com>
2018-05-26 16:35:12 -0300

    Fix search field not full width

--- src/components/HeaderSearch.vue
+++ src/components/HeaderSearch.vue
@@ -21,13 +21,17 @@ export default {
 <style lang="scss" scoped>
 .header-search {
   display: flex;
+  flex: 1;
+
   .search-icon {
     margin-right: 20px;
   }
+
   #search-input {
     font-size: 20px;
     outline: none;
     border: 1px solid transparent;
+    flex: 1;
 
     &:focus {
       border-bottom: 1px solid #0385db;
